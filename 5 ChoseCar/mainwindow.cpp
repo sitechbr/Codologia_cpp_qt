432a67ddb7b8dc17e79bb9b3a6da5c629bee1379
sitechbr <sitech.br@gmail.com>
2023-03-04 21:54:50 +0300

    Added function changed car

--- 5 ChoseCar/mainwindow.cpp
+++ 5 ChoseCar/mainwindow.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QParallelAnimationGroup>
+#include <QPropertyAnimation>
+#include <QSequentialAnimationGroup>
+
 QString car="null";
 
 MainWindow::MainWindow(QWidget *parent)
@@ -14,6 +18,33 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
+void animationCarChange(QLabel *obj) {
+    QPropertyAnimation *animationSize = new QPropertyAnimation(obj, "size");
+    animationSize->setDuration(1000);
+    animationSize->setStartValue(QSize(10, 10));
+
+    animationSize->setEndValue(QSize(391, 211));
+
+
+
+
+    QPropertyAnimation *animationIn = new QPropertyAnimation(obj, "pos");
+    animationIn->setDuration(1000);
+    //animation->setStartValue(QSize(10, 10));
+    animationIn->setStartValue(QPoint(800,260));
+    //animation->setEndValue(QSize(391, 211));
+    animationIn->setEndValue(QPoint(210, 260));
+
+
+    QParallelAnimationGroup *anim_group = new QParallelAnimationGroup();
+    anim_group->addAnimation(animationSize);
+    anim_group->addAnimation(animationIn);
+    anim_group->start();
+}
+
+
 
 
 void MainWindow::on_actionNew_garage_triggered()
@@ -44,19 +75,25 @@ void MainWindow::on_actionLight_Theme_triggered()
 
 void MainWindow::on_b_mers_clicked()
 {
+    animationCarChange(ui->label);
     ui->label->setStyleSheet("border-image:url(\":/rec/img/mercedes_car.png\")");
     car = "Mercedes";
 }
 
 void MainWindow::on_b_bmw_clicked()
 {
+    animationCarChange(ui->label);
     ui->label->setStyleSheet("border-image:url(\":/rec/img/bmw_car.png\")");
     car = "BMW";
 }
 
 void MainWindow::on_b_audi_clicked()
 {
+    animationCarChange(ui->label);
+
     ui->label->setStyleSheet("border-image:url(\":/rec/img/audi_car.png\")");
+
+
     car = "Audi";
 }
 
